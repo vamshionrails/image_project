d8e8b87a95764da8ea92397eb6c7d97aa94bc5c4
vamshi <vamshionrails@gmail.com>
2024-01-21 20:43:00 -0600

    udpated

--- scripts/create_helmrepo.sh
+++ scripts/create_helmrepo.sh
@@ -8,35 +8,17 @@ GITHUB_USERNAME=vamshionrails
 
 
 # Check if the repository already exists
-if [ ! -d helmcharts ]; then
+if [ -d helmcharts ]; then
     # Repository does not exist, initialize a new Helm chart repository
      
     
     # Push the changes to the GitHub repository
     helm create helloworld
     helm package helloworld/ -d pkgs/
-    helm repo index .  --merge index.yaml
+    helm repo index helmcharts --merge index.yaml
     helm package helloworld/ -d pkgs/
     
-else
-
-
-    mkdir ../helm-charts
-    cd ../helm-charts
-    
-
-    # Repository exists, update Helm chart repository and Git repository
-    helm repo index --url https://$GITHUB_USERNAME.github.io/$REPO_NAME --merge index.yaml .
-    
-    # Commit and push the changes to the existing GitHub repository
     
 fi
 
-# Enable GitHub Pages for the repository:
-# - Go to your GitHub repository settings.
-# - Scroll down to the "GitHub Pages" section.
-# - Choose the "master branch" as the source.
-
-# Once GitHub Pages is enabled, your Helm chart repository will be accessible at:
-# https://$GITHUB_USERNAME.github.io/$REPO_NAME
- 
+# 
